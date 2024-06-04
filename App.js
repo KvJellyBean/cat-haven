3bc3f3e035c00f1792a025aaa2b4ed5bee588032
KvJellyBean <kevinnatan91@gmail.com>
2024-06-05 02:02:51 +0700

    feat: add state for splash screen

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
-
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './Screens/LoginScreen';
-import RegisterScreen from './Screens/RegisterScreen';
-import ResetPassword from './Screens/ResetPassword';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SplashScreen from "./Screens/SplashScreen";
+import LoginScreen from "./Screens/LoginScreen";
+import RegisterScreen from "./Screens/RegisterScreen";
+import ResetPassword from "./Screens/ResetPassword";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  const [isSplashScreen, setIsSplashScreen] = React.useState(true);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsSplashScreen(false);
+    }, 3000);
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="ResetPassword" component={ResetPassword} />
-      </Stack.Navigator>
+      {isSplashScreen ? (
+        <SplashScreen />
+      ) : (
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+          <Stack.Screen name="ResetPassword" component={ResetPassword} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
