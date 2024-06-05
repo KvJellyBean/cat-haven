b9d2ab45ca2bd4197d5204078d6a59d75fcd4b07
KvJellyBean <kevinnatan91@gmail.com>
2024-06-05 09:21:54 +0700

    feat: Add onboarding screen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SplashScreen from "./Screens/SplashScreen";
+import OnboardingScreen from "./Screens/OnboardingScreen";
 import LoginScreen from "./Screens/LoginScreen";
 import RegisterScreen from "./Screens/RegisterScreen";
 import ResetPassword from "./Screens/ResetPassword";
@@ -23,9 +24,10 @@ function App() {
         <SplashScreen />
       ) : (
         <Stack.Navigator
-          initialRouteName="Login"
+          initialRouteName="Onboarding"
           screenOptions={{ headerShown: false }}
         >
+          <Stack.Screen name="Onboarding" component={OnboardingScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Register" component={RegisterScreen} />
           <Stack.Screen name="ResetPassword" component={ResetPassword} />
