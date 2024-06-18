e489dbf748a6395f57097f831221121f395bcf4b
KvJellyBean <kevinnatan91@gmail.com>
2024-06-18 15:08:20 +0700

    fix: adjust form modal logic

--- Screens/Form.js
+++ Screens/Form.js
@@ -1,34 +1,47 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView, Modal, Pressable, Alert } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  Modal,
+  Pressable,
+  Alert,
+} from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
-import { collection, doc, setDoc, getDoc } from "firebase/firestore"; // Import necessary Firestore functions
+import { doc, setDoc, getDoc } from "firebase/firestore";
 import { db, auth } from "../firebase";
+import { useNavigation } from "@react-navigation/native";
 
-const AdoptForm = ({ petId, modalVisible, setModalVisible }) => {
+const AdoptForm = ({
+  petId,
+  pet,
+  isVisible,
+  onHide,
+  onFormSubmit,
+  formStatus,
+}) => {
   const navigation = useNavigation();
-
-  const [showDatePicker, setShowDatePicker] = useState(false);
-
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
-    DOB: null, // Tanggal lahir
+    DOB: null,
     typeofpet: "",
     email: "",
     address: "",
-    phoneNumber: "", // Nomor telepon
-    haveChildren: "", // Opsional
+    phoneNumber: "",
+    haveChildren: "",
   });
-
   const [errors, setErrors] = useState({});
-  const [existingForm, setExistingForm] = useState(false);
+  const [showDatePicker, setShowDatePicker] = useState(false);
 
   useEffect(() => {
-    checkExistingForm(); 
+    checkExistingForm();
   }, []);
 
-  const checkExistingForm = async () => { 
+  const checkExistingForm = async () => {
     try {
       const user = auth.currentUser;
       if (!user) return;
@@ -37,23 +50,19 @@ const AdoptForm = ({ petId, modalVisible, setModalVisible }) => {
       const docSnap = await getDoc(userFormRef);
 
       if (docSnap.exists()) {
-        setExistingForm(true);
-      } else {
-        setExistingForm(false);
+        onFormSubmit();
       }
     } catch (error) {
       console.error("Error checking existing form:", error);
     }
-  }
+  };
 
   const handleInputChange = (key, value) => {
-
     if (key === "phoneNumber") {
       value = value.replace(/[^0-9]/g, "");
     }
-
     setFormData({ ...formData, [key]: value });
-    setErrors({ ...errors, [key]: '' });
+    setErrors({ ...errors, [key]: "" });
   };
 
   const validateEmail = (email) => {
@@ -63,46 +72,53 @@ const AdoptForm = ({ petId, modalVisible, setModalVisible }) => {
 
   const handleSubmit = async () => {
     const requiredFields = [
-      { key: 'firstName', label: 'First name' },
-      { key: 'lastName', label: 'Last name' },
-      { key: 'DOB', label: 'Date of birth' },
-      { key: 'typeofpet', label: 'Type of pet' },
-      { key: 'email', label: 'Email' },
-      { key: 'address', label: 'Address' },
-      { key: 'phoneNumber', label: 'Phone number' }
+      { key: "firstName", label: "First name" },
+      { key: "lastName", label: "Last name" },
+      { key: "DOB", label: "Date of birth" },
+      { key: "typeofpet", label: "Type of pet" },
+      { key: "email", label: "Email" },
+      { key: "address", label: "Address" },
+      { key: "phoneNumber", label: "Phone number" },
     ];
     const newErrors = {};
-  
-    requiredFields.forEach(field => {
+
+    requiredFields.forEach((field) => {
       if (!formData[field.key]) {
         newErrors[field.key] = `${field.label} is required`;
       }
     });
-  
+
     if (formData.email && !validateEmail(formData.email)) {
-      newErrors.email = 'Invalid email address';
+      newErrors.email = "Invalid email address";
     }
-  
+
     if (Object.keys(newErrors).length > 0) {
       setErrors(newErrors);
-    } else {
-      try {
-        const user = auth.currentUser;
-        if (!user) return;
-  
-        // Reference to the user's form document for the specific pet
-        const userFormRef = doc(db, "users", user.uid, "form", petId);
-        await setDoc(userFormRef, {
-          ...formData,
-          petId,
-          userId: user.uid,
-        });
-  
-        console.log("Form data submitted successfully");
-        navigation.navigate('CartPageScreen');
-      } catch (error) {
-        console.error("Error submitting form:", error);
-      }
+      return;
+    }
+
+    try {
+      const user = auth.currentUser;
+      if (!user) return;
+
+      const userFormRef = doc(db, "users", user.uid, "form", petId);
+      await setDoc(userFormRef, {
+        ...formData,
+        petId,
+        userId: user.uid,
+      });
+
+      const userCartRef = doc(db, "users", user.uid, "cart", petId);
+      await setDoc(userCartRef, {
+        ...pet,
+      });
+
+      console.log("Form data submitted successfully");
+      onHide();
+      onFormSubmit();
+      navigation.navigate("CartPageScreen", { pet });
+    } catch (error) {
+      console.error("Error submitting form:", error);
     }
   };
 
@@ -110,104 +126,123 @@ const AdoptForm = ({ petId, modalVisible, setModalVisible }) => {
     const currentDate = selectedDate || formData.DOB;
     setShowDatePicker(false);
     setFormData({ ...formData, DOB: currentDate });
-    setErrors({ ...errors, DOB: '' });
+    setErrors({ ...errors, DOB: "" });
   };
 
   const formatDate = (date) => {
     if (!date) return "Select Date of Birth";
-    return date.toLocaleDateString('en-US', {
-      weekday: 'long',
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-    }).replace(/(\d+)\/(\d+)\/(\d+)/, '$2/$1/$3'); // Convert to MM/DD/YYYY
+    return date
+      .toLocaleDateString("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      })
+      .replace(/(\d+)\/(\d+)\/(\d+)/, "$2/$1/$3");
   };
 
   const showAlert = () => {
     Alert.alert(
       "Form Already Submitted",
       "You have already submitted the adoption form for this pet.",
-      [
-        { text: "OK", onPress: () => console.log("OK Pressed") }
-      ],
+      [{ text: "OK" }],
       { cancelable: false }
     );
   };
 
   return (
     <Modal
-      animationType="slide"
+      animationType="fade"
       transparent={true}
-      visible={modalVisible}
-      onRequestClose={() => {
-        setModalVisible(!modalVisible);
-      }}
+      visible={isVisible}
+      onRequestClose={onHide}
     >
       <View style={styles.modalView}>
-        <Pressable style={styles.closeButton} onPress={() => setModalVisible(false)}>
+        <Pressable style={styles.closeButton} onPress={onHide}>
           <Text style={styles.closeText}>×</Text>
         </Pressable>
         <Text style={styles.formTitle}>Adoption Form</Text>
         <ScrollView style={styles.form}>
-          {errors.firstName ? <Text style={styles.errorText}>{errors.firstName}</Text> : null}
+          {errors.firstName && (
+            <Text style={styles.errorText}>{errors.firstName}</Text>
+          )}
           <TextInput
             style={[styles.input, errors.firstName && styles.errorInput]}
             placeholder="First Name"
             onChangeText={(text) => handleInputChange("firstName", text)}
           />
-
-          {errors.lastName ? <Text style={styles.errorText}>{errors.lastName}</Text> : null}
+          {errors.lastName && (
+            <Text style={styles.errorText}>{errors.lastName}</Text>
+          )}
           <TextInput
             style={[styles.input, errors.lastName && styles.errorInput]}
             placeholder="Last Name"
             onChangeText={(text) => handleInputChange("lastName", text)}
           />
-
-          {errors.DOB ? <Text style={styles.errorText}>{errors.DOB}</Text> : null}
-          <TouchableOpacity style={[styles.input, errors.DOB && styles.errorInput]} onPress={() => setShowDatePicker(true)}>
-            <Text style={[styles.dateText, formData.DOB ? styles.dateTextSelected : styles.dateTextPlaceholder]}>
+          {errors.DOB && <Text style={styles.errorText}>{errors.DOB}</Text>}
+          <TouchableOpacity
+            style={[styles.input, errors.DOB && styles.errorInput]}
+            onPress={() => setShowDatePicker(true)}
+          >
+            <Text
+              style={[
+                styles.dateText,
+                formData.DOB
+                  ? styles.dateTextSelected
+                  : styles.dateTextPlaceholder,
+              ]}
+            >
               {formatDate(formData.DOB)}
             </Text>
           </TouchableOpacity>
-
-          {errors.typeofpet ? <Text style={styles.errorText}>{errors.typeofpet}</Text> : null}
+          {errors.typeofpet && (
+            <Text style={styles.errorText}>{errors.typeofpet}</Text>
+          )}
           <TextInput
             style={[styles.input, errors.typeofpet && styles.errorInput]}
             placeholder="Type of Pet"
             onChangeText={(text) => handleInputChange("typeofpet", text)}
           />
-
-          {errors.email ? <Text style={styles.errorText}>{errors.email}</Text> : null}
+          {errors.email && <Text style={styles.errorText}>{errors.email}</Text>}
           <TextInput
             style={[styles.input, errors.email && styles.errorInput]}
             placeholder="Email"
             keyboardType="email-address"
             onChangeText={(text) => handleInputChange("email", text)}
           />
-
-          {errors.address ? <Text style={styles.errorText}>{errors.address}</Text> : null}
+          {errors.address && (
+            <Text style={styles.errorText}>{errors.address}</Text>
+          )}
           <TextInput
             style={[styles.input, errors.address && styles.errorInput]}
             placeholder="Address"
             onChangeText={(text) => handleInputChange("address", text)}
           />
-
-          {errors.phoneNumber ? <Text style={styles.errorText}>{errors.phoneNumber}</Text> : null}
+          {errors.phoneNumber && (
+            <Text style={styles.errorText}>{errors.phoneNumber}</Text>
+          )}
           <TextInput
             style={[styles.input, errors.phoneNumber && styles.errorInput]}
             placeholder="Phone Number"
             keyboardType="number-pad"
-            onChangeText={(text) => handleInputChange("phoneNumber", text.replace(/[^0-9]/g, ''))}
+            onChangeText={(text) =>
+              handleInputChange("phoneNumber", text.replace(/[^0-9]/g, ""))
+            }
           />
-
           <TextInput
             style={styles.input}
             placeholder="Do You Have Children?"
             onChangeText={(text) => handleInputChange("haveChildren", text)}
           />
-
-          <TouchableOpacity style={styles.submitButton} onPress={existingForm ? showAlert : handleSubmit}>
-            <Text style={styles.buttonText}>{existingForm ? "Already Submitted" : "Submit"}</Text>
+          <TouchableOpacity
+            style={[
+              styles.submitButton,
+              formStatus ? styles.disabledButton : null,
+            ]}
+            onPress={formStatus ? showAlert : handleSubmit}
+            disabled={formStatus}
+          >
+            <Text style={styles.buttonText}>Submit</Text>
           </TouchableOpacity>
         </ScrollView>
         {showDatePicker && (
@@ -266,18 +301,15 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 10,
   },
-
-  dateText : {
+  dateText: {
     color: "#626262",
   },
-
   dateTextPlaceholder: {
     color: "#626262",
   },
   dateTextSelected: {
     color: "#000000",
   },
-
   errorInput: {
     borderColor: "red",
   },
@@ -285,7 +317,6 @@ const styles = StyleSheet.create({
     color: "red",
     marginBottom: 10,
   },
-
   submitButton: {
     backgroundColor: "#004AAD",
     padding: 10,
